95f27226e8f5b3b051578fec7a0bbd059f856e02
Steva <asteva@hotmail.co.uk>
2023-01-15 17:47:16 +0000

    Added function which selects 3 random meals

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         return random;
     }
     function chooseProtein() {
-        var proteins = ["Chicken", "Lamb", "Beef", "Eggs", "Tofu", "Pork", "Lentils", "Salmon", "Sardines"];
+        var proteins = ["Chicken", "Lamb", "Beef", "Tofu", "Pork", "Lentils", "Salmon", "Sardines"];
         var proteinChoice = getRandom(proteins);
         var proteinURL = "https://themealdb.com/api/json/v1/1/filter.php?i=" + proteinChoice; 
         // console.log(proteinURL);
@@ -13,7 +13,36 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function (response) {
-                console.log(response);
+                var mealSelection = response.meals; 
+                //select random meals from mealSelection array
+                var meal1 = getRandom(mealSelection); 
+                var meal2 = getRandom(mealSelection); 
+                var meal3 = getRandom(mealSelection); 
+                console.log(meal1); 
+                console.log(meal2); 
+                console.log(meal3);
+                //target headers in html for 3 cards
+                var card1Head = $("#card1-head"); 
+                var card2Head = $("#card2-head"); 
+                var card3Head = $("#card3-head"); 
+                //select names of 3 meals
+                var meal1Name = meal1.strMeal;
+                var meal2Name = meal2.strMeal;
+                var meal3Name = meal3.strMeal;
+                //asign each card a meal name
+                card1Head.text(meal1Name);
+                card2Head.text(meal2Name);
+                card3Head.text(meal3Name); 
+                //select image of 3 meals
+                var card1Img = meal1.strMealThumb; 
+                var card2Img = meal2.strMealThumb; 
+                var card2Img = meal3.strMealThumb;  
+                //target card img 
+                
+                //assign each card an image
+
+
+
              })
         //add code so if array= 2, one card has a standard recipe or is removed
     }
