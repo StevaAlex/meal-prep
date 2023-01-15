a517197145aea630f0776d49282d9771ab688c7f
Steva <asteva@hotmail.co.uk>
2023-01-15 17:53:57 +0000

    Added code for meal images

--- js/script.js
+++ js/script.js
@@ -36,11 +36,16 @@ $(document).ready(function () {
                 //select image of 3 meals
                 var card1Img = meal1.strMealThumb; 
                 var card2Img = meal2.strMealThumb; 
-                var card2Img = meal3.strMealThumb;  
+                var card3Img = meal3.strMealThumb;  
                 //target card img 
-                
+                var meal1img = $("#meal1-img");
+                var meal2img = $("#meal2-img");
+                var meal3img = $("#meal3-img");
                 //assign each card an image
-
+                meal1img.attr("src", card1Img);
+                meal2img.attr("src", card2Img);
+                meal3img.attr("src", card3Img);
+                //find a way to prevent random number generator from selecting same value or same array item
 
 
              })
